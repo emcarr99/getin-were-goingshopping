30b697fe08e39d860c0020a3ac5367ff463473eb
Emma Carr <lorenagracex@gmail.com>
2023-05-12 09:36:48 -0500

    get product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,8 +6,36 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+     attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    // be sure to include its associated Category and Tag data
+     include: [
+      {
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: [
+          'id',
+          'tag_name'
+        ]
+      }
+    ]
+  }).then((productData) => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
+
+  
 
 // get one product
 router.get('/:id', (req, res) => {
